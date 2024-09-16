d77ade82fd040e0da1a919769d872154bd8a1294
Ayush Goyal <93977033+Ayushgoyal0803@users.noreply.github.com>
2024-09-16 07:18:43 +0530

    Create App.js

---
+++ fruit-ai-frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
+import Chatbot from './Chatbot';
+import Translator from './Translator';
+import FAQ from './Faq';
+import About from './About';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/translator" element={<Translator />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
